c28804aaa5939f03c592e7de6c253a727029b2e6
KricsDavid <david@kricsfalusi.hu>
2024-12-04 09:31:54 +0100

    nem jo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-
-import UserCard from "./usercard/Usercard";
+import { useEffect, useState } from "react";
+import UserCard from "Usercard";
+import usersData from "../public/users.json";
 import "./app.css";
-
+type User = {
+  id: number;
+  name: string;
+  email: string;
+  age: number;
+  profilePicture: string;
+};
 function App() {
+
+  function App() {
+    const [users, setUsers] = useState<User[]>([]);
+    const [searchTerm, setSearchTerm] = useState<string>("");
+    const [errorMessage, setErrorMessage] = useState<string>("");
+  
+    useEffect(() => {
+      setUsers(usersData);
+
+
   return (
     <div>
       <h1>User Information</h1>
---
+++ src/UserCard.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Image from "next/image";
+
+type UserCardProps = {
+  user: User;
+};
+
+const UserCard: React.FC<UserCardProps> = ({ user }) => {
+  return (
+    <div className="user-card">
+      <div className="profile-picture">
+        <Image
+          width={100}
+          height={100}
+          alt={`${user.name} profile picture`}
+          src={user.profilePicture}
+          className="profile-image"
+        />
+      </div>
+      <div className="user-info">
+        <h3>{user.name}</h3>
+        <p>Email: {user.email}</p>
+        <p>Age: {user.age}</p>
+      </div>
+    </div>
+  );
+};
+
+export default UserCard;
